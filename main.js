c6cd5dde137ade3719365f325f14b0be27779d6b
Greg Marsella <gkmarsella@gmail.com>
2016-10-28 14:02:50 -0700

    Added buttons

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ $(function(){
 		var topTen = data.slice(0,10);
 		topTen.forEach(function(el) {
 			$.get("https://hacker-news.firebaseio.com/v0/item/" + el + ".json").then(function(data){
-				($("ol").append($('<div class="topTen">')).append($('<button type="button" id="btn">&#9734</button>')).append($('<a>').attr('href', (data.url)).append((data.title) + (" | by: " + (data.by).append))));
+				var appDiv = '<div data-url="' + data.url + '" class="topTen">' + '<button type="button">&#9734</button>' + '<a href="' + data.url + '">' + data.title  + " | by: " + data.by;
+				$("#mainDiv").append(appDiv);
 			});
 		});
 
@@ -13,7 +14,26 @@ $(function(){
 
 
 
+	$("#mainDiv").on('click', function(event){
+		if($(event.target).is(":button")){
+			event.preventDefault();
+			$(".favoriteList").append(this);
+		}
+	});
 });
 
 
 
+
+// $(function(){
+// 	$.get("https://hacker-news.firebaseio.com/v0/topstories.json").then(function(data){
+// 		console.log(data);
+// 		var topTen = data.slice(0,10);
+// 		topTen.forEach(function(el) {
+// 			$.get("https://hacker-news.firebaseio.com/v0/item/" + el + ".json").then(function(data){
+// 				($("#mainDiv").append($('<div data-url="' + data.url + '" class="topTen"></div)')).append(
+// 					$('<button type="button">&#9734</button>')).append($('<a>').attr('href', (data.url)).append((data.title) + " | by: " + data.by)));
+// 			});
+// 		});
+
+// 	});
