23fd9fd857fcbfc0c0d528471b344f87b282b2f8
Greg Marsella <gkmarsella@gmail.com>
2016-10-28 15:11:47 -0700

    added click to add to favorites

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(function(){
 		var topTen = data.slice(0,10);
 		topTen.forEach(function(el) {
 			$.get("https://hacker-news.firebaseio.com/v0/item/" + el + ".json").then(function(data){
-				var appDiv = '<div data-url="' + data.url + '" class="topTen">' + '<button type="button">&#9734</button>' + '<a href="' + data.url + '">' + data.title  + " | by: " + data.by;
+				var appDiv = '<div data-id="' + data.id + '" data-by="' + data.by + '" data-title="' + data.title + '" data-url="' + data.url + '" class="topTen">' + '<button type="button">&#9734</button>' + '<a href="' + data.url + '">' + data.title  + " | by: " + data.by;
 				$("#mainDiv").append(appDiv);
 			});
 		});
@@ -17,23 +17,20 @@ $(function(){
 	$("#mainDiv").on('click', function(event){
 		if($(event.target).is(":button")){
 			event.preventDefault();
-			$(".favoriteList").append(this);
+			$(event.target).html("&#9733");
+			var id = $(event.target).parent().data("id");
+			var by = $(event.target).parent().data("by");
+			var title = $(event.target).parent().data("title");
+			var url = $(event.target).parent().data("url");
+			$.post("https://hn-favorites.herokuapp.com/stories.json", {"hacker_news_story":{"title": title, "by": by, "story_id": id, "url": url}}).then(function(response){
+	   			console.log(response);
+			});
 		}
 	});
 });
 
 
-
-
-// $(function(){
-// 	$.get("https://hacker-news.firebaseio.com/v0/topstories.json").then(function(data){
-// 		console.log(data);
-// 		var topTen = data.slice(0,10);
-// 		topTen.forEach(function(el) {
-// 			$.get("https://hacker-news.firebaseio.com/v0/item/" + el + ".json").then(function(data){
-// 				($("#mainDiv").append($('<div data-url="' + data.url + '" class="topTen"></div)')).append(
-// 					$('<button type="button">&#9734</button>')).append($('<a>').attr('href', (data.url)).append((data.title) + " | by: " + data.by)));
-// 			});
-// 		});
-
-// 	});
+// $.post("https://hn-favorites.herokuapp.com/stories.json", {"hacker_news_story":{"Title": data-id, "Author": data-by, "Id": data-id, "URL": data-url}}).then(function(response){
+//    console.log(response);
+//    console.log("Hello")
+// });
